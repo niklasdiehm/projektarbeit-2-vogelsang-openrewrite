1b3784be7b665e301692cdf98cc342602aae580f
Niklas Diehm <niklas.diehm@highspeed-karlsruhe.de>
2023-10-05 21:35:28 +0200

    Imports weden gelöscht und sortiert

--- demoanwendung/src/main/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplication.java
+++ demoanwendung/src/main/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplication.java
@@ -3,8 +3,6 @@ package com.hka.niklasdiehm.demoanwendung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
 public class DemoanwendungApplication {
 
--- demoanwendung/src/test/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplicationTests.java
+++ demoanwendung/src/test/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplicationTests.java
@@ -1,6 +1,7 @@
 package com.hka.niklasdiehm.demoanwendung;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
