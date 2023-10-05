d15caa918ff82097881f86aed6756a87522897d1
Niklas Diehm <niklas.diehm@highspeed-karlsruhe.de>
2023-10-05 20:40:03 +0200

    Initialer Dateienupload

---
+++ demoanwendung/src/main/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplication.java
@@ -0,0 +1,13 @@
+package com.hka.niklasdiehm.demoanwendung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoanwendungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoanwendungApplication.class, args);
+	}
+
+}
---
+++ demoanwendung/src/test/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hka.niklasdiehm.demoanwendung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoanwendungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
