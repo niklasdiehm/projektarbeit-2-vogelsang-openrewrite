21051a8c237067440c736a95b87dbd0291d1ce39
Niklas Diehm <niklas.diehm@highspeed-karlsruhe.de>
2023-10-05 21:32:11 +0200

    Unnötigen Import hinzugefügt

--- demoanwendung/src/main/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplication.java
+++ demoanwendung/src/main/java/com/hka/niklasdiehm/demoanwendung/DemoanwendungApplication.java
@@ -3,6 +3,8 @@ package com.hka.niklasdiehm.demoanwendung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 public class DemoanwendungApplication {
 
